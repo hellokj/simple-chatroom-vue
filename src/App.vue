d79b5766b1d75b5d1a7c7d36fdab85aa41046020
hellokjtw <hellokjtw@gmail.com>
2021-11-11 19:58:38 +0800

    sytle: format the code

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,74 @@
 <template>
   <div>
     <div>
-      <el-button type="success" class="button-wrapper" :disabled="isConnected" @click="connect">Connect</el-button>
-      <el-button type="success" class="button-wrapper" :disabled="!isConnected" @click="disconnect">Disonnect</el-button>
+      <el-button
+        type="success"
+        class="button-wrapper"
+        :disabled="isConnected"
+        @click="connect"
+        >Connect</el-button
+      >
+      <el-button
+        type="success"
+        class="button-wrapper"
+        :disabled="!isConnected"
+        @click="disconnect"
+        >Disonnect</el-button
+      >
     </div>
     <div>
-      <el-input class="input-wrapper" :disabled="!isConnected||isJoined" v-model="room" placeholder="Please input the room name." />
-      <el-input class="input-wrapper" :disabled="!isConnected||isJoined" v-model="name" placeholder="Please input your name." />
-      <el-button @click="join" :disabled="!isConnected||isJoined">Join</el-button>
-      <el-button @click="leave" :disabled="!isConnected||!isJoined">Leave</el-button>
+      <el-input
+        class="input-wrapper"
+        :disabled="!isConnected || isJoined"
+        v-model="room"
+        placeholder="room name"
+      />
+      <el-input
+        class="input-wrapper"
+        :disabled="!isConnected || isJoined"
+        v-model="name"
+        placeholder="your name"
+      />
+      <el-button @click="join" :disabled="!isConnected || isJoined"
+        >Join</el-button
+      >
+      <el-button @click="leave" :disabled="!isConnected || !isJoined"
+        >Leave</el-button
+      >
     </div>
     <div class="message-panel">
-      <msgBox v-for="(item, index) of msgList" :key="index+Math.random()" :uname="item.name" :content="item.msg" :isself="item.isSelf" :isFromSystem="item.isFromSystem"></msgBox>
+      <msgBox
+        v-for="(item, index) of msgList"
+        :key="index + Math.random()"
+        :uname="item.name"
+        :content="item.msg"
+        :isself="item.isSelf"
+        :isFromSystem="item.isFromSystem"
+      ></msgBox>
     </div>
     <div>
-      <el-input :disabled="!isConnected||!isJoined" class="content" v-model="content" placeholder="Type something : " />
-      <el-button :disabled="!isConnected||!isJoined" @click="send" type="primary">Send</el-button>
+      <el-input
+        :disabled="!isConnected || !isJoined"
+        class="content"
+        v-model="content"
+        placeholder="Type something : "
+      />
+      <el-button
+        :disabled="!isConnected || !isJoined"
+        @click="send"
+        type="primary"
+        >Send</el-button
+      >
     </div>
   </div>
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-import msgBox from '../src/components/MsgBox.vue';
+import msgBox from "../src/components/MsgBox.vue";
 export default {
-  name: 'App',
-  data(){
-    return{
+  name: "App",
+  data() {
+    return {
       websocket: null,
       msgList: [],
       room: "",
@@ -35,37 +76,37 @@ export default {
       isConnected: false,
       isJoined: false,
       clientId: "",
-      content: ""
-    }
+      content: "",
+    };
   },
   components: {
     // HelloWorld
-    msgBox
+    msgBox,
   },
-  methods:{
-    connect(){
-      const wsurl = 'ws://localhost:8080/websocket';
+  methods: {
+    connect() {
+      const wsurl = "ws://localhost:8080/websocket";
       this.websocket = new WebSocket(wsurl);
       this.websocket.onopen = this.onWsOpen;
       this.websocket.onerror = this.onWsError;
       this.websocket.onmessage = this.onWsMessage;
       this.websocket.onclose = this.onWsClose;
     },
-    disconnect(){
-      if (this.websocket){
+    disconnect() {
+      if (this.websocket) {
         // before disconnect, it should be leave the room first.
         this.leave();
         this.websocket.close();
         this.websocket = null;
       }
     },
-    join(){
+    join() {
       // check the name and room content
       // lock the name and room input
-      if (Array.isArray(this.msgList) && this.msgList.length){
+      if (Array.isArray(this.msgList) && this.msgList.length) {
         this.msgList = [];
       }
-      if (this.name && this.room){
+      if (this.name && this.room) {
         this.isJoined = true;
 
         const joinMessage = {
@@ -73,49 +114,49 @@ export default {
           Action: "Join",
           RoomId: this.room,
           NickName: this.name,
-          Content: this.content
-        }
+          Content: this.content,
+        };
         this.sendMessageToServer(joinMessage);
-      }else{
+      } else {
         alert("Should fill the both room and name info.");
       }
     },
-    leave(){
+    leave() {
       const leaveMessage = {
         ClientId: this.clientId,
         Action: "Leave",
         RoomId: this.room,
         NickName: this.name,
-        Content: this.content
-      }
+        Content: this.content,
+      };
       this.sendMessageToServer(leaveMessage);
       this.isJoined = false;
     },
-    send(){
+    send() {
       // send the message to server
-      if (this.content){
+      if (this.content) {
         const broadcastMessage = {
           ClientId: this.clientId,
           Action: "Broadcast",
           RoomId: this.room,
           NickName: this.name,
-          Content: this.content
-        }
+          Content: this.content,
+        };
         this.sendMessageToServer(broadcastMessage);
         this.content = "";
-      }else{
+      } else {
         alert("You can't send empty content.");
       }
     },
-    sendMessageToServer(message){
+    sendMessageToServer(message) {
       console.log(JSON.stringify(message));
       this.websocket.send(JSON.stringify(message));
     },
-    onWsOpen(){
+    onWsOpen() {
       alert("Success");
       this.isConnected = true;
     },
-    onWsMessage(e){
+    onWsMessage(e) {
       const resdata = JSON.parse(e.data);
       console.log(resdata);
       this.clientId = resdata.ClientId;
@@ -123,13 +164,13 @@ export default {
         name: resdata.From,
         msg: resdata.Content,
         isFromSystem: resdata.IsFromSystem,
-        isSelf: resdata.FromId == this.clientId
-      })
+        isSelf: resdata.FromId == this.clientId,
+      });
     },
-    onWsError(){
+    onWsError() {
       alert("Connect failed. Please check the websocket server.");
     },
-    onWsClose(){
+    onWsClose() {
       alert("Disconnected.");
       // init the data
       this.isConnected = false;
@@ -138,41 +179,46 @@ export default {
       this.room = "";
       this.content = "";
       this.clientId = "";
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style scoped>
-  .button-wrapper{
-    margin: 1%;
-  }
+.button-wrapper {
+  margin: 1%;
+}
 
-  .input-wrapper{
-    width: 20%;
-    margin: 1%;
-  }
+.input-wrapper {
+  width: 20%;
+  margin: 1%;
+}
 
-  .message-panel{
-    width: 90%;
-    height: 550px;
-    border: 1px #572d2d solid;
-    overflow: scroll;
-    overflow-x: hidden;
-    padding: 10px;
-    margin: 1%;
-  }
+.message-panel::-webkit-scrollbar {
+  display: none;
+}
 
-  .content{
-    margin: 1%;
-    width: 90%;
-  }
+.message-panel {
+  width: 90%;
+  height: 550px;
+  border: 1px #572d2d solid;
+  overflow: scroll;
+  overflow-x: hidden;
+  scrollbar-width: none;
+  padding: 10px;
+  margin: 10px;
+}
 
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
+.content {
+  margin: 1%;
+  width: 90%;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
--- src/components/MsgBox.vue
+++ src/components/MsgBox.vue
@@ -1,52 +1,49 @@
 <template>
-
-    <div class="msg-wrapper" v-bind:class="getClass()">
-        <span v-if="!isFromSystem && !isself" class="uname">{{uname}}</span>
-        <span v-if="!isFromSystem && !isself">：</span>
-        <div class="content">{{content}}</div>
-    </div>
- 
+  <div class="msg-wrapper" v-bind:class="getClass()">
+    <span v-if="!isFromSystem && !isself" class="uname">{{ uname }}</span>
+    <span v-if="!isFromSystem && !isself">：</span>
+    <div class="content">{{ content }}</div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'msgBox',
+  name: "msgBox",
   props: {
     isFromSystem: Boolean,
     uname: String,
     content: String,
-    isself: Boolean
+    isself: Boolean,
   },
-  methods:{
-      getClass(){
-          if (this.isFromSystem) return "system";
-          if (this.isself) return "self";
-          return "";
-      }
-  }
-}
+  methods: {
+    getClass() {
+      if (this.isFromSystem) return "system";
+      if (this.isself) return "self";
+      return "";
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .msg-wrapper {
-    display: flex;
-    width: 100%;
-    flex-direction: row;
-    padding: 10px;
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+  padding: 10px;
 }
 .content {
-    border-radius: 2px;
-    margin-left: 1%;
-    margin-right: 1%;
+  border-radius: 2px;
+  margin-left: 10px;
+  margin-right: 10px;
 }
 .self {
-    flex-direction: row-reverse;
+  flex-direction: row-reverse;
 }
 
-.system{
-    color: gray;
-    justify-content: center
+.system {
+  color: gray;
+  justify-content: center;
 }
-  
 </style>    
